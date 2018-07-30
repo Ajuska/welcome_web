33ace931f6bcf1be3ba61b3106e2b95326b8b584
Andrea Kopecna <andrea.kopecna@ixperta.com>
2018-07-30 10:55:13 +0200

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         driver_path = "/home/andreakopecna/Projects/welcome_web/drivers/geckodriver"
@@ -21,79 +21,79 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    # def test_can_start_a_list_and_retrieve_it_later(self):
-    #     # Edith has heard about a cool new online to-do app. She goes
-    #     # to check out its homepage
-    #     self.browser.get(self.live_server_url)
-    #
-    #     # She notices the page title and header mention to-do lists
-    #     self.assertIn('To-Do', self.browser.title)
-    #     header_text = self.browser.find_element_by_tag_name('h1').text
-    #     self.assertIn('To-Do', header_text)
-    #
-    #     # She is invited to enter a to-do item straight away
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     self.assertEqual(
-    #         inputbox.get_attribute('placeholder'),
-    #         'Enter a to-do item'
-    #     )
-    #
-    #     # She types "Buy peacock feathers" into a text box (Edith's hobby
-    #     # is tying fly-fishing lures)
-    #     inputbox.send_keys('Buy peacock feathers')
-    #
-    #     # When she hits enter, t she is taken to a new URL,
-    #     # and now the page lists "1: Buy peacock feathers" as an item in a
-    #     # to-do list table
-    #     inputbox.send_keys(Keys.ENTER)
-    #     ### it needs to wait so it loads correcly the current URL
-    #     time.sleep(1)
-    #     edith_list_url = self.browser.current_url
-    #     self.assertRegex(edith_list_url, '/lists/.+')
-    #     self.check_for_row_in_list_table('1: Buy peacock feathers')
-    #
-    #     # There is still a text box inviting her to add another item. She
-    #     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     inputbox.send_keys('Use peacock feathers to make a fly')
-    #     inputbox.send_keys(Keys.ENTER)
-    #
-    #     # The page updates again, and now shows both items on her list
-    #     self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-    #     self.check_for_row_in_list_table('1: Buy peacock feathers')
-    #
-    #     # Now new user, Francis, comes along to the site
-    #
-    #     ## We use a new browser session to make sure that no information of
-    #     ## Edith's is comming through from cookies TestCase
-    #     self.browser.quit()
-    #     driver_path = "/home/andreakopecna/Projects/welcome_web/drivers/geckodriver"
-    #     self.browser = webdriver.Firefox(executable_path=driver_path)
-    #
-    #     # Francis visits the home page. There is no sign of Edith's lists
-    #     self.browser.get(self.live_server_url)
-    #     page_text = self.browser.find_element_by_tag_name('body').text
-    #     self.assertNotIn('Buy peacock feathers', page_text)
-    #     self.assertNotIn('make a fly', page_text)
-    #
-    #     # Francis starts a new list vy entering a new item. He is les interesting
-    #     # than Edith...
-    #     inputbox = self.browser.find_element_by_id('id_new_item')
-    #     inputbox.send_keys('Buy milk')
-    #     inputbox.send_keys(Keys.ENTER)
-    #
-    #     # Francis gets his own unique URL
-    #     time.sleep(1)
-    #     francis_list_url = self.browser.current_url
-    #     self.assertRegex(francis_list_url, '/lists/.+')
-    #     self.assertNotEqual(francis_list_url, edith_list_url)
-    #
-    #     # Again, there is no trace of Edith's lists
-    #     page_text = self.browser.find_element_by_tag_name('body').text
-    #     self.assertNotIn('Buy peacock feathers', page_text)
-    #     self.assertIn('Buy milk', page_text)
-    #
-    #     # Satisfied, they both go back to sleep
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, t she is taken to a new URL,
+        # and now the page lists "1: Buy peacock feathers" as an item in a
+        # to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        ### it needs to wait so it loads correcly the current URL
+        time.sleep(1)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Now new user, Francis, comes along to the site
+
+        ## We use a new browser session to make sure that no information of
+        ## Edith's is comming through from cookies TestCase
+        self.browser.quit()
+        driver_path = "/home/andreakopecna/Projects/welcome_web/drivers/geckodriver"
+        self.browser = webdriver.Firefox(executable_path=driver_path)
+
+        # Francis visits the home page. There is no sign of Edith's lists
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list vy entering a new item. He is les interesting
+        # than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets his own unique URL
+        time.sleep(1)
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's lists
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
